b4a9c2db92277cf49e3fd13c7a964a684262b467
Miguel Braga Gomes <miguel.bgomes@gmail.com>
2021-05-08 18:50:13 +0100

    initial

--- js/index.js
+++ js/index.js
@@ -1,35 +1,24 @@
 const cards = [
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' },
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' }
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" }
 ];
 
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards();
 
-window.addEventListener('load', event => {
-  let html = '';
-  memoryGame.cards.forEach(pic => {
+window.addEventListener("load", (event) => {
+  let html = "";
+  memoryGame.cards.forEach((pic) => {
     html += `<div class="card" data-card-name="${pic.name}">`;
     html += `<div class="back" name="${pic.img}"></div>`;
     html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
@@ -37,13 +26,34 @@ window.addEventListener('load', event => {
   });
 
   // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
+  document.querySelector("#memory-board").innerHTML = html;
 
   // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+  document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("click", () => {
+      if (memoryGame.pickedCards.length < 2) {
+        card.classList.add("turned");
+        memoryGame.pickedCards.push(card);
+      }
+
+      if (memoryGame.pickedCards.length === 2) {
+        const pair1 =
+          memoryGame.pickedCards[0].attributes["data-card-name"].value;
+        const pair2 =
+          memoryGame.pickedCards[1].attributes["data-card-name"].value;
+        if (!memoryGame.checkIfPair(pair1, pair2)) {
+          setTimeout(() => {
+            memoryGame.pickedCards.forEach((_card) => {
+              _card.classList.remove("turned");
+              memoryGame.pickedCards = [];
+            });
+          }, 1000);
+        } else {
+          document.getElementById("pairs-guessed").innerText = memoryGame.pairsGuessed;
+          memoryGame.pickedCards = [];
+        }
+        document.getElementById("pairs-clicked").innerText  = memoryGame.pairsClicked;
+      }
     });
   });
 });
--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,41 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    if (!this.cards) {
+      return undefined;
+    }
+
+    let currentIndex = this.cards.length,
+      temporaryValue,
+      randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+    return this.cards;
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    return false;
+  }
+  isFinished() {
+    if (this.cards.length / 2 === this.pairsGuessed) {
+      return true
+    }
+    return false;
+  }
 }
